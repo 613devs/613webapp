684f77cca3437b05d93255f8c73fe340773f9690
calvinvanoss <cvanoss@calpoly.edu>
2023-12-22 16:34:35 -0800

    deprecate firebase to 9 and init sveltefire and fix line endings

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,41 +1,33 @@
 <script lang="ts">
-	import { onMount } from 'svelte';
+	import { FirebaseApp } from 'sveltefire';
+	import { initializeApp } from 'firebase/app';
 	import '../app.css';
 	import Navbar from '../components/Navbar.svelte';
-	import { authStore } from '../store/store';
-	import { auth } from '$lib/firebaseConfig';
-	import { doc, getDoc } from 'firebase/firestore';
-	import { db } from '$lib/firebaseConfig';
-	import type { TUser } from '../types';
+	import { connectFirestoreEmulator, getFirestore } from 'firebase/firestore';
+	import { connectAuthEmulator, getAuth } from 'firebase/auth';
+	import { getDatabase } from 'firebase/database';
+	import { getStorage } from 'firebase/storage';
 
-	onMount(() => {
-		const unsubscribe = auth.onAuthStateChanged(async (user) => {
-			if (!user) {
-				authStore.update(() => {
-					return {
-						googleUser: null,
-						dbUser: null,
-					};
-				});
-			} else {
-				const docRef = doc(db, 'users', user.uid);
-				const docSnap = await getDoc(docRef);
-				if (docSnap.exists()) {
-					const dbUser = docSnap.data() as TUser;
-					authStore.update(() => {
-						return {
-							googleUser: user,
-							dbUser: dbUser,
-						};
-					});
-				}
-			}
-		});
-		return unsubscribe;
+	const app = initializeApp({
+		apiKey: import.meta.env.VITE_API_KEY,
+		authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+		projectId: import.meta.env.VITE_PROJECT_ID,
+		storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+		messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+		appId: import.meta.env.VITE_APP_ID,
+		measurementId: import.meta.env.VITE_MEASUREMENT_ID,
 	});
+	const firestore = getFirestore(app);
+	const auth = getAuth(app);
+	if (typeof location !== 'undefined' && location.hostname === 'localhost') {
+		connectFirestoreEmulator(firestore, 'localhost', 8080);
+		connectAuthEmulator(auth, 'http://localhost:9099');
+	}
+	const rtdb = getDatabase(app);
+	const storage = getStorage(app);
 </script>
 
-<div>
+<FirebaseApp {firestore} {auth} {rtdb} {storage}>
 	<Navbar />
 	<slot />
-</div>
+</FirebaseApp>
