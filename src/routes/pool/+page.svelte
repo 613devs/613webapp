9797acd95ab8907a720f3f2c8532a36d77864382
oli-lane <markus.oliver.lane@gmail.com>
2023-12-26 17:28:02 -0800

    style: more unused code

--- src/routes/pool/+page.svelte
+++ src/routes/pool/+page.svelte
@@ -1,10 +1,3 @@
-<script lang="ts">
-	import { getFirebaseContext, userStore } from 'sveltefire';
-
-	const { auth } = getFirebaseContext();
-	// const user = userStore(auth!);
-</script>
-
 <main>
 	<div class="container mx-auto px-30 py-5 flex flex-col gap-10 min-h-screen">
 		<div class="bg-accent-content h-80 rounded-badge flex flex-col items-center">
