d090e9136291c8229f427ac706aa39c7897527eb
oli-lane <markus.oliver.lane@gmail.com>
2023-12-26 17:21:14 -0800

    feat: navbar resizing, pool page start

--- src/components/Navbar.svelte
+++ src/components/Navbar.svelte
@@ -8,11 +8,15 @@
 
 {#if auth}
 	<header class="navbar">
-		<div class="navbar-start prose">
-			<h1>613 Stanford Dr</h1>
+		<div class="navbar-start flex gap-2">
+			<div class="prose">
+				<h1>613</h1>
+			</div>
+			<div class="prose hidden md:block">
+				<h1>Stanford Dr</h1>
+			</div>
 		</div>
-
-		<div class="navbar-center">
+		<div class="navbar-center space-x-2">
 			<a class="btn" href="/">Home</a>
 			<a class="btn" href="/blog" data-sveltekit-preload-data>Blog</a>
 			<a class="btn" href="/pool" data-sveltekit-preload-data>Pool</a>
@@ -21,7 +25,7 @@
 		<div class="navbar-end space-x-1">
 			{#if $user}
 				<Doc ref={`profiles/${$user.uid}`} let:data>
-					<p class="prose">
+					<p class="prose hidden md:block">
 						Welcome, <span class="italic font-bold">
 							{data.username}
 						</span>
@@ -33,7 +37,7 @@
 							class="h-12 w-12 rounded-full"
 						/>
 					</a>
-					<button on:click={logout} class="btn">Log Out</button>
+					<button on:click={logout} class="btn hidden md:block">Log Out</button>
 				</Doc>
 			{:else}
 				<button on:click={login} class="btn">
--- src/routes/profile/+page.svelte
+++ src/routes/profile/+page.svelte
@@ -1,17 +1,19 @@
 <script lang="ts">
 	import { Doc, getFirebaseContext, userStore } from 'sveltefire';
+    import {logout } from '$lib/helpers/auth';
 
 	const { auth } = getFirebaseContext();
 	const user = userStore(auth!);
 </script>
 
 <Doc ref={`profiles/${$user?.uid}`} let:data>
-	<div class="flex justify-center align-middle min-h-screen bg-base-200">
+	<div class="flex flex-col items-center mx-auto min-h-screen bg-base-200">
 		<div class="text-center prose my-5">
 			<h3 class="text-7xl">{data.username}</h3>
 			<p>
 				{data.role} since {data.created_dt.toDate().toLocaleDateString()}
 			</p>
 		</div>
+        <button on:click={logout} class="btn bg-neutral-content">Log Out</button>
 	</div>
 </Doc>
