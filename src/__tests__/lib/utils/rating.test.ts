32502ab70d96bcf664e80629cb84f197c3374fd9
oli-lane <markus.oliver.lane@gmail.com>
2023-12-28 13:52:52 -0800

    chore: cleanup calculateRatings

--- src/__tests__/lib/utils/rating.test.ts
+++ src/__tests__/lib/utils/rating.test.ts
@@ -1,38 +1,32 @@
-import { calculateNewRatings } from '$lib/utils/rating';
+import { calculateRatings } from '$lib/utils/rating';
 import { expect, test } from 'vitest';
 
 test('calculateNewRatings correctly calculates simple rating changes', () => {
-	const playerARating = 1000;
-	const playerBRating = 1000;
-	const playerAResult = 1; // playerA wins
+	const winnerRating = 1000;
+	const loserRating = 1000;
 
-	const result = calculateNewRatings(playerARating, playerBRating, playerAResult);
+	const results = calculateRatings(winnerRating, loserRating);
 
-	const expectedPlayerANewRating = 1016;
-	const expectedPlayerAChange = 16;
-	const expectedPlayerBNewRating = 984;
-	const expectedPlayerBChange = -16;
+	const winnerNewRating = 1016;
+	const loserNewRating = 984;
+	const change = 16;
 
-	expect(result.playerANewRating).toBe(expectedPlayerANewRating);
-	expect(result.playerAChange).toBe(expectedPlayerAChange);
-	expect(result.playerBNewRating).toBe(expectedPlayerBNewRating);
-	expect(result.playerBChange).toBe(expectedPlayerBChange);
+	expect(results.winnerNewRating).toBe(winnerNewRating);
+	expect(results.loserNewRating).toBe(loserNewRating);
+	expect(results.change).toBe(change);
 });
 
 test('calculateNewRatings correctly calculates complex rating changes', () => {
-	const playerARating = 1231;
-	const playerBRating = 1078;
-	const playerAResult = 0; // playerB wins
+	const winnerRating = 1078;
+	const loserRating = 1231;
 
-	const result = calculateNewRatings(playerARating, playerBRating, playerAResult);
+	const results = calculateRatings(winnerRating, loserRating);
 
-	const expectedPlayerANewRating = 1208;
-	const expectedPlayerAChange = -23;
-	const expectedPlayerBNewRating = 1101;
-	const expectedPlayerBChange = 23;
+	const winnerNewRating = 1101;
+	const loserNewRating = 1208;
+	const change = 23;
 
-	expect(result.playerANewRating).toBe(expectedPlayerANewRating);
-	expect(result.playerAChange).toBe(expectedPlayerAChange);
-	expect(result.playerBNewRating).toBe(expectedPlayerBNewRating);
-	expect(result.playerBChange).toBe(expectedPlayerBChange);
+	expect(results.winnerNewRating).toBe(winnerNewRating);
+	expect(results.loserNewRating).toBe(loserNewRating);
+	expect(results.change).toBe(change);
 });
--- src/lib/utils/rating.ts
+++ src/lib/utils/rating.ts
@@ -2,55 +2,15 @@ const EXPONENT_DENOMINATOR = 400;
 const EXPONENT_BASE = 10;
 const K_FACTOR = 32;
 
-const calculateExpectedProbability = (ratingDifference: number) => {
-	const exponent = ratingDifference / EXPONENT_DENOMINATOR;
-	return 1 / (1 + Math.pow(EXPONENT_BASE, exponent));
-};
-
-const calculateMatchProbability = (playerARating: number, playerBRating: number) => {
-	const ratingADifference = playerBRating - playerARating;
-	const ratingBDifference = playerARating - playerBRating;
-
-	const playerAExpectedProbability = calculateExpectedProbability(ratingADifference);
-	const playerBExpectedProbability = calculateExpectedProbability(ratingBDifference);
-
-	return [playerAExpectedProbability, playerBExpectedProbability];
-};
-
-const calculateNewRating = (rating: number, result: number, expectedProbability: number) => {
-	const change = Math.round(K_FACTOR * (result - expectedProbability));
-	const newRating = rating + change;
-	return [newRating, change];
-};
-
-export const calculateNewRatings = (
-	playerARating: number,
-	playerBRating: number,
-	playerAResult: number,
-) => {
-	const [playerAExpectedProbability, playerBExpectedProbability] = calculateMatchProbability(
-		playerARating,
-		playerBRating,
-	);
-
-	// playerAResult = 1 if playerA wins, 0 if playerB wins
-	const playerBResult = 1 - playerAResult;
-
-	const [playerANewRating, playerAChange] = calculateNewRating(
-		playerARating,
-		playerAResult,
-		playerAExpectedProbability,
-	);
-	const [playerBNewRating, playerBChange] = calculateNewRating(
-		playerBRating,
-		playerBResult,
-		playerBExpectedProbability,
-	);
-
+export const calculateRatings = (winnerRating: number, loserRating: number) => {
+	const winnerExpected =
+		1 / (1 + Math.pow(EXPONENT_BASE, (loserRating - winnerRating) / EXPONENT_DENOMINATOR));
+	const change = Math.round(K_FACTOR * (1 - winnerExpected));
+	const winnerNewRating = winnerRating + change;
+	const loserNewRating = loserRating - change;
 	return {
-		playerANewRating,
-		playerAChange,
-		playerBNewRating,
-		playerBChange,
+		winnerNewRating,
+		loserNewRating,
+		change,
 	};
 };
