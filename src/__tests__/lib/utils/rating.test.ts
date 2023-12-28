79476824ee7b90f1771b2960d47e441a87ba370f
calvinvanoss <cvanoss@calpoly.edu>
2023-12-28 13:48:14 -0800

    add rating system

---
+++ src/__tests__/lib/utils/rating.test.ts
@@ -0,0 +1,22 @@
+import { calculateNewRatings } from '$lib/utils/rating';
+import { expect, test } from 'vitest';
+
+test('calculateNewRatings correctly calculates simple rating changes', () => {
+	const winnerRating = 1000;
+	const loserRating = 1000;
+
+	const result = calculateNewRatings(winnerRating, loserRating);
+
+	expect(result.newWinnerRating).toBe(1016);
+	expect(result.newLoserRating).toBe(984);
+});
+
+test('calculateNewRatings correctly calculates complex rating changes', () => {
+	const winnerRating = 1078;
+	const loserRating = 1231;
+
+	const result = calculateNewRatings(winnerRating, loserRating);
+
+	expect(result.newWinnerRating).toBe(1101);
+	expect(result.newLoserRating).toBe(1208);
+});
---
+++ src/lib/utils/rating.ts
@@ -0,0 +1,18 @@
+const EXPONENT_DENOMINATOR = 400;
+const EXPONENT_BASE = 10;
+const K_FACTOR = 32;
+
+export const calculateNewRatings = (winnerRating: number, loserRating: number) => {
+	const difference = Math.abs(winnerRating - loserRating);
+
+	const expectedProbability = 1 / (1 + Math.pow(EXPONENT_BASE, difference / EXPONENT_DENOMINATOR));
+
+	const newWinnerRating = winnerRating + Math.round(K_FACTOR * (1 - expectedProbability));
+
+	const newLoserRating = loserRating - Math.round(K_FACTOR * (1 - expectedProbability));
+
+	return {
+		newWinnerRating,
+		newLoserRating,
+	};
+};
