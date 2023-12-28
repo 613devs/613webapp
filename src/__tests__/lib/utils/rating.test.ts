7699ba5da83ae842034e0929c2b54ca880319ae2
calvinvanoss <cvanoss@calpoly.edu>
2023-12-28 13:49:34 -0800

    rm newlines

--- src/__tests__/lib/utils/rating.test.ts
+++ src/__tests__/lib/utils/rating.test.ts
@@ -4,7 +4,6 @@ import { expect, test } from 'vitest';
 test('calculateNewRatings correctly calculates simple rating changes', () => {
 	const winnerRating = 1000;
 	const loserRating = 1000;
-
 	const result = calculateNewRatings(winnerRating, loserRating);
 
 	expect(result.newWinnerRating).toBe(1016);
@@ -14,7 +13,6 @@ test('calculateNewRatings correctly calculates simple rating changes', () => {
 test('calculateNewRatings correctly calculates complex rating changes', () => {
 	const winnerRating = 1078;
 	const loserRating = 1231;
-
 	const result = calculateNewRatings(winnerRating, loserRating);
 
 	expect(result.newWinnerRating).toBe(1101);
--- src/lib/utils/rating.ts
+++ src/lib/utils/rating.ts
@@ -4,11 +4,8 @@ const K_FACTOR = 32;
 
 export const calculateNewRatings = (winnerRating: number, loserRating: number) => {
 	const difference = Math.abs(winnerRating - loserRating);
-
 	const expectedProbability = 1 / (1 + Math.pow(EXPONENT_BASE, difference / EXPONENT_DENOMINATOR));
-
 	const newWinnerRating = winnerRating + Math.round(K_FACTOR * (1 - expectedProbability));
-
 	const newLoserRating = loserRating - Math.round(K_FACTOR * (1 - expectedProbability));
 
 	return {
