401c0d0be2827805ffbf9b36fb8e013f08349462
calvinvanoss <cvanoss@calpoly.edu>
2023-12-28 11:23:20 -0800

    add todo tests for everything

--- src/__tests__/example.test.ts
+++
@@ -1,5 +0,0 @@
-import { expect, test } from 'vitest';
-
-test('vitest example', () => {
-	expect(true).toBe(true);
-});
--- src/__tests__/lib/components/Navbar.svelte.test.ts
+++ src/__tests__/lib/components/Navbar.svelte.test.ts
@@ -5,7 +5,6 @@ import Navbar from '$lib/components/Navbar.svelte';
 
 describe('Navbar.svelte', () => {
 	it('should render logged in', () => {
-		mockState.isUserSignedIn = true;
 		const { container } = render(Navbar);
 		expect(container).toMatchSnapshot();
 	});
@@ -14,5 +13,6 @@ describe('Navbar.svelte', () => {
 		mockState.isUserSignedIn = false;
 		const { container } = render(Navbar);
 		expect(container).toMatchSnapshot();
+		mockState.isUserSignedIn = true;
 	});
 });
