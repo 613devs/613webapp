7b784082bec7a777dd5a7e9ea1218c0b781b0b31
oli-lane <markus.oliver.lane@gmail.com>
2024-01-03 19:04:09 -0800

    rebased

--- src/routes/pool/+page.svelte
+++ src/routes/pool/+page.svelte
@@ -1,31 +1,11 @@
 <script lang="ts">
-	import type { PageData } from './$types';
-
-	export let data: PageData;
-</script>
-
-<div class="container mx-auto max-w-screen-lg">
-	<div class="mx-auto text-center prose my-3">
-		<h3 class="text-4xl">Leaderboard</h3>
-	</div>
-	<table class="table">
-		<tr class="prose">
-			<th class="text-left">username</th>
-			<th class="text-right">rating</th>
-		</tr>
-		{#each data.profiles as profile}
-			<tr class="hover prose">
-				<td class="text-left">{profile.username}</td>
-				<td class="text-right">{profile.rating}</td>
-			</tr>
-		{/each}
-	</table>
-	import { Doc, SignedIn, SignedOut, getFirebaseContext, userStore } from 'sveltefire';
-	import { addDoc, collection, doc, updateDoc } from 'firebase/firestore';
 	import { SignedIn } from 'sveltefire';
 	import { addDoc, collection } from 'firebase/firestore';
 	import { firestore } from '$lib/firebase';
 	import Modal from '../../lib/components/Modal.svelte';
+	import type { PageData } from './$types';
+
+	export let data: PageData;
 
 	// logging a match
 	let showModal = false;
@@ -126,6 +106,18 @@
 			<div class="text-center prose my-3">
 				<h3 class="text-4xl">Leaderboard</h3>
 			</div>
+			<table class="table">
+				<tr class="prose">
+					<th class="text-left">username</th>
+					<th class="text-right">rating</th>
+				</tr>
+				{#each data.profiles as profile}
+					<tr class="hover prose">
+						<td class="text-left">{profile.username}</td>
+						<td class="text-right">{profile.rating}</td>
+					</tr>
+				{/each}
+			</table>
 		</div>
 		<div class="bg-accent-content h-20 rounded-badge flex flex-col items-center">
 			<div class="text-center prose my-3">
